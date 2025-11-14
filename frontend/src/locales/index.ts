ee79db6d3a9212bb265a5f67f491f1076cb64716
amarpay <hi@imzami.com>
2025-11-14 20:49:21 +0400

    fix delete language issue

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -1,5 +1,9 @@
 import { createI18n } from 'vue-i18n'
-import en from './en' 
+import en from './en'
+import fa from './fa'
+import vi from './vi'
+import zhcn from './zhcn'
+import zhtw from './zhtw'
 import ru from './ru'
 
 export const i18n = createI18n({
@@ -8,13 +12,19 @@ export const i18n = createI18n({
   fallbackLocale: 'en',
   messages: {
     en: en,
-
+    fa: fa,
+    vi: vi,
+    zhHans: zhcn,
+    zhHant: zhtw,
     ru: ru
   },
 })
 
 export const languages = [
   { title: 'English', value: 'en' },
- 
+  { title: 'فارسی', value: 'fa' },
+  { title: 'Tiếng Việt', value: 'vi' },
+  { title: '简体中文', value: 'zhHans' },
+  { title: '繁體中文', value: 'zhHant' },
   { title: 'Русский', value: 'ru' },
 ]
